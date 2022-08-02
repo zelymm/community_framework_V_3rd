2e7a2f1fe4081cb553c3f4df63834bc32a93867f
zelymm <dkdlwkrz@naver.com>
2022-08-02 10:53:37 +0900

    added annotation

---
+++ src/main/java/com/ll/exam/Container.java
@@ -0,0 +1,14 @@
+package com.ll.exam;
+
+import com.ll.exam.article.controller.ArticleController;
+
+public class Container {
+    private static final ArticleController articleController;
+
+    static {
+        articleController = new ArticleController();
+    }
+    public static ArticleController getArticleController() {
+        return articleController;
+    }
+}
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -3,6 +3,7 @@ package com.ll.exam;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ll.exam.article.controller.ArticleController;
 
 public class AppTest {
     @Test
@@ -11,4 +12,18 @@ public class AppTest {
 
         assertThat(rs).isEqualTo(30);
     }
+    @Test
+    public void ioc__articleController() {
+        ArticleController articleController = Container.getArticleController();
+
+        assertThat(articleController).isNotNull();
+    }
+    @Test
+    public void ioc__articleController__싱글톤() {
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController2).isEqualTo(articleController1);
+    }
 }
+
